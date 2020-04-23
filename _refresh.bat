46689f69db829fec1375261c7f9b959c63e1d64a
Adam Stevenson <stevenson_adam@yahoo.com>
2020-04-23 09:11:55 -0600

    Refresh bundles without replacing DataRequirements

--- _refresh.bat
+++ _refresh.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 SET tooling_jar=tooling-1.0.2-SNAPSHOT-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
+SET resources_path=%~dp0/input/resources
 
 ECHO Checking internet connection...
 PING tx.fhir.org -n 1 -w 1000 | FINDSTR TTL && GOTO isonline
@@ -17,11 +18,11 @@ SET fsoption=-fs http://cqm-sandbox.alphora.com/cqf-ruler-r4/fhir/
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 IF EXIST "%input_cache_path%\%tooling_jar%" (
-	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
-	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
+	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -iv=fhir4 -t -d -p %fsoption%
+	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -iv=fhir4 -t -d -p %fsoption%
 ) ELSE If exist "..\%tooling_jar%" (
-	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
-	JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -t -d -p -v %fsoption%
+	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -iv=fhir4 -t -d -p %fsoption%
+	JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -rp="%resources_path%" -t -d -p %fsoption%
 ) ELSE (
 	ECHO IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 )
