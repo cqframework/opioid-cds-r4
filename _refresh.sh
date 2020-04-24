fe4eacb109540fb053bb28f511ab06d3df7670ce
Adam Stevenson <stevenson_adam@yahoo.com>
2020-04-24 14:42:17 -0600

    Refreshed with proper canonical URLs for relatedArtifacts; modified refresh script to pass path to ig resource arg.

--- _refresh.sh
+++ _refresh.sh
@@ -3,6 +3,7 @@
 tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
 resources_path=./input/resources
+ig_resource_path=./input/opioid-cds.xml
 
 set -e
 echo Checking internet connection...
@@ -21,14 +22,14 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -iv=fhir4 -t -d -p $fsoption
-	#JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -iv=fhir4 -t -d -p -v $fsoption
+	JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p $fsoption
+	#JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p -v $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -rp="$resources_path" -iv=fhir4 -t -d -p $fsoption
-		#JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -rp="$resources_path" -iv=fhir4 -t -d -p -v $fsoption
+		JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p $fsoption
+		#JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p -v $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
