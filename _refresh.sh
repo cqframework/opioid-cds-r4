685242a4edc752d74a31f6191c92ac4b2bc38c91
Adam Stevenson <stevenson_adam@yahoo.com>
2020-04-28 20:00:31 -0600

    Fixed refresh script

--- _refresh.sh
+++ _refresh.sh
@@ -2,7 +2,7 @@
 #DO NOT EDIT WITH WINDOWS
 tooling_jar=tooling-1.1.0-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
-resources_path=./input/resources
+resources_path=$PWD/input/resources
 ig_resource_path=./input/opioid-cds.xml
 
 set -e
@@ -22,14 +22,12 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p $fsoption
-	#JAVA -jar $tooling -RefreshIG -ip="$PWD" -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p -v $fsoption
+	JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir4 -t -d -p $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p $fsoption
-		#JAVA -jar $tooling -RefreshIG -ip=C%~dp0 -rp="$resources_path" -igrp="$ig_resource_path" -iv=fhir4 -t -d -p -v $fsoption
+		JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir4 -t -d -p $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
