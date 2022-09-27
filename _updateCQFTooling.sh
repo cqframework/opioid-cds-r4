a974901a55443aa51f1510dd62824860b79685b1
c-schuler <hoofschu@gmail.com>
2022-09-26 22:53:24 -0600

    Updated scripts to use new cli tooling jar ... refreshed test data and artifacts

--- _updateCQFTooling.sh
+++ _updateCQFTooling.sh
@@ -4,16 +4,15 @@
 
 r=snapshots
 g=org.opencds.cqf
-a=tooling
-v=1.4.1-SNAPSHOT
-c=jar-with-dependencies
+a=tooling-cli
+v=2.1.0-SNAPSHOT
 
-dlurl='https://oss.sonatype.org/service/local/artifact/maven/redirect?r='${r}'&g='${g}'&a='${a}'&v='${v}'&c='${c}''
+dlurl='https://oss.sonatype.org/service/local/artifact/maven/redirect?r='${r}'&g='${g}'&a='${a}'&v='${v}''
 
 echo ${dlurl}
 
 input_cache_path=./input-cache/
-tooling_jar=tooling-1.4.1-SNAPSHOT-jar-with-dependencies.jar
+tooling_jar=tooling-cli-2.1.0-SNAPSHOT.jar
 
 set -e
 if ! type "curl" > /dev/null; then
@@ -54,7 +53,6 @@ fi
 read -r -p "$message" response
 if [[ "$response" =~ ^([yY])$ ]]; then
 	echo "Downloading most recent tooling to $jarlocationname - it's ~170 MB, so this may take a bit"
-#	wget "https://oss.sonatype.org/service/local/repositories/snapshots/content/org/opencds/cqf/tooling/1.0-SNAPSHOT/tooling-1.0-20200107.163002-6-jar-with-dependencies.jar" -O "$jarlocation"
 	curl $dlurl -L -o "$jarlocation" --create-dirs
 	echo "Download complete."
 else
